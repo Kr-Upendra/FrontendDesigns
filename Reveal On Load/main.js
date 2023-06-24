7762afcf8344f834eb6ceba1be10b6b9c731fc8d
Upendra Kumar <toxic74412@gmail.com>
2023-06-25 00:37:03 +0530

    added theme changing function

--- Reveal On Load/main.js
+++ Reveal On Load/main.js
@@ -13,3 +13,34 @@ const reveal = () => {
 };
 
 window.addEventListener("scroll", reveal);
+
+const night = document.querySelector(".toggle__theme");
+const body = document.querySelector("body");
+const headings = document.querySelectorAll(".section__heading--h2");
+
+const onNightMode = () => {
+  body.style.backgroundColor = "#242323";
+  headings.forEach((heading) => {
+    heading.style.color = "var(--primary)";
+    night.style.backgroundColor = "var(--primary)";
+  });
+};
+
+const onLightMode = () => {
+  body.style.backgroundColor = "var(--primary)";
+  headings.forEach((heading) => {
+    heading.style.color = "var(--dark)";
+    night.style.backgroundColor = "var(--dark)";
+  });
+};
+
+night.addEventListener("click", () => {
+  const isNight = night.getAttribute("data-isnight");
+  if (isNight === "true") {
+    night.setAttribute("data-isnight", "false");
+    onLightMode();
+  } else {
+    night.setAttribute("data-isnight", "true");
+    onNightMode();
+  }
+});
